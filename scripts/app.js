2db98c1644c21a73f8c61ff30036a732dc670b24
Caleb-Cove <coveneycn@s.dcsdk12.org>
2021-11-19 09:48:15 -0700

    S T I M U L A T I O N

--- scripts/app.js
+++ scripts/app.js
@@ -6,31 +6,79 @@ const ctx = canvas.getContext('2d');
 canvas.width = 1200;
 canvas.height = 700;
 
-class bal {
+function parabollicEasing(pt) {
+    let x = pt * 4 - 2;
+    let y= x * x*-1 +4; //((x^2)*-1) +4
+    return y/4;
+}
+
+class KeyboardState {
+    constructor() {
+        this.isAccelerating =false;
+        this.isBraking = false;
+        this.registerEventHandlers();
+    }
+
+    registerEventHandlers() {
+        window.addEventListener("keydown", (e) => {
+            switch (e.key) {
+                case "a":
+                case "ArrowLeft":
+                    this.isBraking = true;
+                    break;
+                case "d":
+                case "ArrowRight":
+                    this.isAccelerating=true;
+                    break;
+            }
+        });
+        window.addEventListener("keyup", (e) => {
+            switch (e.key) {
+                case "a":
+                case "ArrowLeft":
+                    this.isBraking = false;
+                    break;
+                case "d":
+                case "ArrowRight":
+                    this.isAccelerating=  false;
+                    break;
+            
+            }   
+        })
+    }
+};  
+
+class Bal {
     constructor() {
         this.X = canvas.width * .15;
         this.yLastBounce=0;
         this.Y = -400;
-        this.speed = 20;
+        //this.speed = 10;
+        this.bounceTime = 1750;
+        this.timeSinceLastBounce= 0;
         this.radius = 20;
         this.maxBounceHeight=canvas.height/2;
+        
     }
 
 
-    update() {
-        const isMovingDown = this.speed > 0;
+    update(timeElapsed) {
+        this.timeSinceLastBounce+=timeElapsed;
+        const isMovingDown = this.timeSinceLastBounce > this.bounceTime/2;
 
-        this.Y = this.Y + this.speed;
+
+        let ef = parabollicEasing(this.timeSinceLastBounce/this.bounceTime)
+        this.Y = this.yLastBounce - this.maxBounceHeight * ef;
 
         if(this.Y+this.radius >= canvas.height) {
-            this.speed *= -1;
+            this.timeSinceLastBounce=0;
             this.yLastBounce = this.Y;
         }
 
-        if(!isMovingDown && 
-            this.Y <= this.yLastBounce+this.maxBounceHeight) {
-            this.speed *= -1;
-        }
+        // if(!isMovingDown && 
+        //     this.Y <= this.yLastBounce-this.maxBounceHeight) {
+        //     // this.speed *= -1;
+        // }
     }
 
     render() {
@@ -43,14 +91,123 @@ class bal {
     }
 };
 
-let player = new bal();
+class Game {
+    /**
+     * @param {KeyboardState} kb
+     */
+    constructor(kb) {
+        this.kb=kb;
+        this.speed= 0;
+        this.maxSpeed= 100;
+        this.accelerationRate = 5;
+        this.accelerationInterval = 100;
+        this.timeSinceLastAcceleration = 0;
+    }
+    update(timeElapsed) {
+        this.timeSinceLastAcceleration += timeElapsed;
 
-function gameLoop() {
+        if(this.kb.isAccelerating && this.speed < this.maxSpeed &&
+            this.timeSinceLastAcceleration >= this.accelerationInterval
+            ) {
+                this.speed += this.accelerationRate;
+                this.timeSinceLastAcceleration = 0;
+
+        }
+
+        if(this.kb.isBraking) {
+            this.speed = 0;
+            this.timeSinceLastAcceleration = 0;
+        }
+
+        if(!this.kb.isAccelerating && !this.kb.isBraking &&
+            this.timeSinceLastAcceleration >= this.accelerationInterval
+            && this.speed > 0) {
+            //decelerate
+            this.speed -= this.accelerationRate;
+            this.timeSinceLastAcceleration = 0;
+        }
+
+    }
+
+    render() {}
+};
+
+class Tracer {
+    /**
+     * @param {Bal} b
+     * @param {Game} g
+    */
+    constructor(b, g){
+        this.b = b;
+        this.g = g;
+
+        this.x = b.X;
+        this.y = b.Y;
+        
+        this.isVisible= true;
+        this.opacity= 1;
+
+        this.fadeRate = .1;
+        this.fadeInterval = 100;
+        this.timeSincLastFade = 0;
+    }
+        update(timeElapsed) {
+        this.timeSincLastFade += timeElapsed;
+        this.x -= this.g.speed;
+        
+        if(this.timeSincLastFade >= this.fadeInterval) {
+            this.opacity -=this.fadeRate;
+            this.timeSincLastFade = 0;
+        }
+
+        this.isVisible = this.opacity > 0;
+    }
+
+    render() {
+        ctx.save();
+        ctx.fillStyle = `hsla(0, 0%, 50%, ${this.opacity})`;
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.b.radius/2, 0, Math.PI*2, true) 
+        ctx.fill();
+
+        ctx.restore();
+    }
+    
+}
+
+let player = new Bal(); 
+let kb= new KeyboardState();
+let game= new Game(kb);
+let tracers = [new Tracer(player, game)]
+
+
+let currentTime = 0;
+
+
+function gameLoop(timestamp) {
+    let timeElapsed = timestamp - currentTime;
+    currentTime = timestamp;
+
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    player.update();
+    
+
+    game.update(timeElapsed);
+    game.render();
+
+    tracers.push(new Tracer(player, game));
+
+    tracers.forEach((t) => {
+        t.update(timeElapsed);
+        t.render();
+    })
+
+    player.update(timeElapsed);
     player.render();
+
+    tracers = tracers.filter(t => t.isVisible);
 
     requestAnimationFrame(gameLoop)
 };
 
-requestAnimationFrame(gameLoop)
+
+    requestAnimationFrame(gameLoop)
